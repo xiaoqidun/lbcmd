05320a9d877914ac153d23a027662396983a9c5b
xiaoqidun <xiaoqidun@gmail.com>
2016-08-10 02:30:17 +0800

    xiaoqidun_lbcmd

---
+++ xiaoqidun_lbcmd/xiaoqidun_lbcmd.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace xiaoqidun_lbcmd
+{
+    partial class xiaoqidun_lbcmd
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(xiaoqidun_lbcmd));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.爱特网络ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.技术社区ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.ContextMenuStrip = this.contextMenuStrip1;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(172, 80);
+            this.panel1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.爱特网络ToolStripMenuItem,
+            this.技术社区ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(100, 48);
+            // 
+            // 爱特网络ToolStripMenuItem
+            // 
+            this.爱特网络ToolStripMenuItem.Name = "爱特网络ToolStripMenuItem";
+            this.爱特网络ToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.爱特网络ToolStripMenuItem.Text = "爱特网络";
+            this.爱特网络ToolStripMenuItem.Click += new System.EventHandler(this.爱特网络ToolStripMenuItem_Click);
+            // 
+            // 技术社区ToolStripMenuItem
+            // 
+            this.技术社区ToolStripMenuItem.Name = "技术社区ToolStripMenuItem";
+            this.技术社区ToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.技术社区ToolStripMenuItem.Text = "技术社区";
+            this.技术社区ToolStripMenuItem.Click += new System.EventHandler(this.技术社区ToolStripMenuItem_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "移除bash右键";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "设置bash右键";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // xiaoqidun_lbcmd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::xiaoqidun_lbcmd.Properties.Resources.BackgroundImage;
+            this.ClientSize = new System.Drawing.Size(196, 104);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "xiaoqidun_lbcmd";
+            this.Text = "爱特bash右键";
+            this.Load += new System.EventHandler(this.xiaoqidun_lbcmd_Load);
+            this.panel1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem 爱特网络ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 技术社区ToolStripMenuItem;
+    }
+}
+
---
+++ xiaoqidun_lbcmd/xiaoqidun_lbcmd.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace xiaoqidun_lbcmd
+{
+    public partial class xiaoqidun_lbcmd : Form
+    {
+        public string bash = @"C:\Windows\System32\bash.exe";
+        public string lbcmd = @"Directory\Background\shell\lbcmd";
+        public string lbcmd_icon = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\lxss\bash.ico";
+        public xiaoqidun_lbcmd()
+        {
+            InitializeComponent();
+        }
+
+        private void xiaoqidun_lbcmd_Load(object sender, EventArgs e)
+        {
+            Activate();
+            TopMost = true;
+            if (!File.Exists(bash))
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var key_exists = Registry.ClassesRoot.OpenSubKey(lbcmd);
+            if (key_exists == null)
+            {
+                var key = Registry.ClassesRoot.CreateSubKey(lbcmd);
+                key.SetValue(null, "在此处打开子系统命令窗口");
+                key.SetValue("icon", lbcmd_icon);
+                key.Close();
+                var key_command = Registry.ClassesRoot.CreateSubKey(lbcmd + @"\command");
+                key_command.SetValue(null, bash);
+                key_command.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var key_exists = Registry.ClassesRoot.OpenSubKey(lbcmd);
+            if (key_exists != null)
+            {
+                Registry.ClassesRoot.DeleteSubKeyTree(lbcmd, false);
+            }
+        }
+
+        private void 爱特网络ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://aite.me/");
+        }
+
+        private void 技术社区ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://webthen.net/");
+        }
+    }
+}
